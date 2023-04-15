b5cb2f1c78e2275fde3ab477dce87ab51ce7b534
BrianKopp <briankopp.usa@gmail.com>
2023-04-15 16:41:26 -0600

    Add options for tab things

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -3,9 +3,11 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 -- Set up indendation
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.softtabstop = 0
 vim.opt.autoindent = true
 -- vim.opt.backspace = indent,eol,start
 
