268d3cf9a8cf4089f319612a5a5f1be8eabbd91f
Brian Kopp <briankopp.usa@gmail.com>
2023-04-24 13:29:49 -0600

    Update LSP config

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ require('mason').setup()
 require('mason-lspconfig').setup({ automatic_installation = true })
 
 -- Global mappings
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
