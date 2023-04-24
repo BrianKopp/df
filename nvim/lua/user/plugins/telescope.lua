fb6a21a74e92d2a8e6045efc7e3de6c246bd6d11
Brian Kopp <briankopp.usa@gmail.com>
2023-04-24 13:29:17 -0600

    Add key binding for telescope resume

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -33,4 +33,5 @@ vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]])
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>r', builtin.resume, {})
 
