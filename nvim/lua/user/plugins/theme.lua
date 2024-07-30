71fae89f20912984d21a30176b5198c07ff85767
Brian Kopp <brian@bigeye.com>
2024-07-30 08:55:30 -0600

    Update theme

--- nvim/lua/user/plugins/theme.lua
+++ nvim/lua/user/plugins/theme.lua
@@ -1,9 +1,15 @@
-require('tokyonight').setup({
-  style = "night",
-  on_colors = function(colors)
-    colors.fg_gutter = "#8da6b8"
-  end,
+-- require('tokyonight').setup({
+--   style = "night",
+--   on_colors = function(colors)
+--     colors.fg_gutter = "#8da6b8"
+--   end,
+-- })
+
+-- vim.cmd[[colorscheme tokyonight-night]]
+
+require("catppuccin").setup({
+  flavour = "mocha",
+  -- flavour = "latte",
 })
 
-vim.cmd[[colorscheme tokyonight-night]]
-
+vim.cmd[[colorscheme catppuccin]]
